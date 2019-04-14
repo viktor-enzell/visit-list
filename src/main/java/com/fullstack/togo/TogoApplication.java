efb3fe0343e4ea82486442b1406faa7274247fbe
viktor-enzell <viktor.enzell@gmail.com>
2019-04-14 12:40:17 +0200

    Init Maven project using the Spring Initializer webpage.

---
+++ src/main/java/com/fullstack/togo/TogoApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.togo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogoApplication.class, args);
+	}
+
+}
