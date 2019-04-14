aa20bdbc8d13bb6af8685272a01793599c720aa6
viktor-enzell <viktor.enzell@gmail.com>
2019-04-14 21:44:04 +0200

    Added HTML requests for adding, retrieving and removing locations from a hashmap in the backend server.

--- src/main/app/src/App.js
+++ src/main/app/src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
             lng: 5,
             lat: 34,
             zoom: 1.5,
+            locations: ["London", "Paris", "Istanbul"]
         };
     }
 
@@ -35,27 +36,52 @@ class App extends Component {
         });
 
         map.on('click', (e) => {
-            const marker = new mapboxgl.Marker()
+            new mapboxgl.Popup()
               .setLngLat(e.lngLat)
+              .setHTML('<input type="text"/><button>Save location</button>')
               .addTo(map);
+            this.setLocation("Paris", "LONG LAT");
+            this.getLocation("Paris")
         });
     }
 
-    hello = () => {
-        this.setState({message: "New Hello!"});
-        /*
-        fetch('/api/hello')
+    setLocation(name, lngLat) {
+        fetch('/api/setLocation',
+          {
+              method: 'POST',
+              body: JSON.stringify({
+                  name: name,
+                  lngLat: lngLat,
+              }).then
+          });
+    }
+
+    getLocation(name) {
+        fetch('/api/getLocation', {method: 'POST', body: name})
           .then(response => response.text())
           .then(message => {
               this.setState({message: message});
           });
-         */
-    };
+    }
+
+    removeLocation(name) {
+        fetch('/api/removeLocation', {method: 'DELETE', body: name})
+          .then(response => response.text())
+          .then(message => {
+              this.setState({message: message});
+          });
+    }
 
     render() {
         return (
           <div className="App">
               <div ref={el => this.mapContainer = el} className="map"/>
+              <div>
+                  {this.state.locations.map(item =>
+                    <p key={item.toString()}>{item}</p>
+                  )}
+              </div>
+              <p>{this.state.message}</p>
           </div>
         );
     }
--- src/main/java/com/fullstack/togo/Controller.java
+++ src/main/java/com/fullstack/togo/Controller.java
@@ -1,12 +1,34 @@
 package com.fullstack.togo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class Controller {
-    @GetMapping("/api/hello")
-    public String hello() {
-        return "Hello!\n";
+
+    private Map<String, String> locations = new HashMap<>();
+
+    @RequestMapping(value = "/api/setLocation", method = RequestMethod.POST)
+    public void setLocation(@RequestBody String bodyString) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode body = mapper.readTree(bodyString);
+        String name = body.get("name").asText();
+        String lngLat = body.get("lngLat").asText();
+        locations.put(name, lngLat);
+    }
+
+    @RequestMapping(value = "/api/getLocation", method = RequestMethod.POST)
+    public String getLocation(@RequestBody String name) {
+        return locations.get(name);
+    }
+
+    @RequestMapping(value = "/api/removeLocation", method = RequestMethod.DELETE)
+    public void removeLocation(@RequestBody String name) {
+        locations.remove(name);
     }
 }
